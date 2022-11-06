31ba29fe4bcfe6b5dbf76e973e78fad3fa5eb552
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-11-06 00:55:07 -0300

    Validate (Model) - Validating a user's email with validates method and DB configuration

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2022_11_06_025447) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_processed"
+    t.string "email", null: false
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
